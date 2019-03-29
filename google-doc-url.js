e0767cf085c8f09db2b9db9d515736c5c3689b87
Filippo Maria Raeli <filippo.raeli@gmail.com>
2019-03-29 16:11:44 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1P5SOKmqjp74qbdj64kMB4XLgINIztqb6D3ncHiio6Eo';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
